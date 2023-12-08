11bdf29efd200ecedcd588e521452b55a50ea8f7
monkCommits <speak2monk@gmailcom>
2023-12-08 08:09:16 +0530

    settingUp

---
+++ 1.3/index.js
@@ -0,0 +1,28 @@
+var myName = "   kartikey tiwari"
+
+function getLength(string) {
+    console.log(string, " ",string.length);
+}
+
+function findInexOf(string) {
+    console.log(string.indexOf('k'));
+    console.log(string.lastIndexOf('k'));
+}
+
+function getSlice(string) {
+    console.log(string.slice(1,4));
+}
+
+function split(string) {
+    console.log(string.split(" "));
+}
+function trim(string) {
+    console.log(string.trim());
+}
+
+substring
+replace
+toUpperCase
+toLowerCase
+
+trim(myName)
